f6c9f727bce7517e1fccb99192a5ebb94feca5db
Ventilateur <mrfantasiospirou@gmail.com>
2015-10-23 09:54:12 +0200

    This has been changed recently

---
+++ Pilotes/gpio/gpio.h
@@ -0,0 +1,19 @@
+#include "stm32f10x.h"
+
+// permet de configurer une broche d'un port en output push-pull
+char Port_IO_Init_Output(GPIO_TypeDef * Port, u8 Broche);
+
+// permet de configurer une broche d'un port en input floating
+char Port_IO_Init_Input(GPIO_TypeDef * Port, u8  Broche);
+
+// permet de mettre à 1 une broche d'un port
+void Port_IO_Set(GPIO_TypeDef * Port, u8 Broche);
+
+// permet de mettre à 0 une broche d'un port
+void Port_IO_Reset(GPIO_TypeDef * Port, u8 Broche);
+
+// inverse l'état d'une broche
+void Port_IO_Blink(GPIO_TypeDef * Port, u8 Broche);
+
+// permet de lire l'état d'une broche d'un port
+unsigned int Port_IO_Read(GPIO_TypeDef * Port, u8 Broche);
